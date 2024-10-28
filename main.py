5d7650de32edbf31f6976e4754dca139a078c727
LiborZak <libor.zak.zn@gmail.com>
2024-10-28 19:21:58 +0100

    Vytvořena apka, ale zatím nejde převést na exe

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
-# Naimportujte tkinter
-# Vytvořte jedno okno o rozměru 500 na 500 a nastavte hlavní cyklus
-# Do okna přidejte ikonku s názvem icon.ico.
-# Nastavte popisek okna "Přepočet kurzu"
+from  tkinter import  *
 
-# Vytvořte label s textem "Eura" a názvem currency, jeho font nastavte na Cambria, 20 pixelů, tučný (bold)
-# Barvu pozadí celého okna! nastavte na černou
-# Barvu pozadí labelu nastavte na černou
-# Barvu textu labelu nastavte na bílou (fg)
+# plátno
+canvas = Tk()
+canvas.title("Převod měn z CZK na EUR")
+canvas.minsize(250, 200)
+canvas.resizable(False, False)
+canvas.config(bg="#072a00")
+canvas.iconbitmap("icon.ico")
 
-import tkinter
-# nastavení plátna
-window = tkinter.Tk()
-window.minsize(500, 500)
-window.resizable(False, False)
-window.iconbitmap("pictures/icon_ratecalc.ico")
-window.title("Přepočet kurzu")
-window.config(bg="black")
+# kurz v eurech
+kurz = 25.36
 
-# Label
-currency = tkinter.Label(text="Eura", font=("Cambria", 20, "bold"), bg="black", fg="white")
-currency.pack()
+# funkce pro výpočet
+def count_course():
+    czk = float(input_amount.get())
+    label_result["text"] = round(czk / kurz, 2)
+    input_amount.delete(0, END)
 
-window.mainloop()
+# vstup
+input_amount = Entry(width=10, font=("Helvetica", 15))
+input_amount.grid(row=0, column=0, padx=10, pady=10)
+
+#štítky
+label_CZK = Label(text="CZK", font=("Helvetica", 15))
+label_CZK.grid(row=0, column=1)
+label_CZK.config(bg="#072a00", fg="white")
+
+label_result = Label(text="0", font=("Helvetica", 15))
+label_result.grid(row=1, column=0)
+label_result.config(bg="#072a00", fg="white")
+
+label_EUR = Label(text="EUR", font=("Helvetica", 15))
+label_EUR.grid(row=1, column=1)
+label_EUR.config(bg="#072a00", fg="white")
+
+# tlačítko
+count_result = Button(text="Převést", font=("Helvetica", 15), command=count_course)
+count_result.grid(row=0, column=2, padx=10, pady=10)
+
+# hlavní cyklus
+canvas.mainloop()
